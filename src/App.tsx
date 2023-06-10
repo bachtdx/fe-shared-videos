78388693cb467d33a899e35f4ba2168f5cf56f7b
BachTDX <truongdoxuanbach@gmail.com>
2023-06-10 22:46:56 +0700

    [ValidateToken] add validate token

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Layout } from "antd";
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import Home from "./pages/home";
 import Share from "./pages/share";
+import Cookies from "js-cookie";
+import axios from "./utils/axiosUtils";
 const { Content } = Layout;
 
 interface User {
@@ -14,12 +16,28 @@ interface User {
 }
 function App() {
   const [user, setUser] = useState<User>({
-    token: "a",
+    token: "",
     email: "",
   });
+  useEffect(() => {
+    const token = Cookies.get("token");
+    console.log(token);
+    const response = axios
+      .post("/api/v1/validation_token", {})
+      .then((response) => {
+        console.log("Success:", response.data);
+        setUser({
+          token: token || "",
+          email: response.data.userEmail,
+        })
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
   return (
     <Layout>
-      <Header user={user} />
+      <Header user={user} setUser={setUser} />
       <Content>
         <BrowserRouter>
           <Routes>
